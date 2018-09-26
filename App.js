d2245234ecdfa0171b0e2792f28722884dc17205
Mariana Oliveira <marianaoliveira.unilasalle@gmail.com>
2018-09-26 13:44:54 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ class HomeScreen extends React.Component {
       <View style={styles.container}>
       <View style={styles.welcomecontainer}>
         <Text style={styles.paragraph}>
-         Seja Bem vindo a AV1.
+         Olá, bem vindo a AV1.
         </Text>
          <Text style={styles.welcomeDescription}>
 		    Por favor, clique no botão abaixo.
@@ -41,13 +41,13 @@ class DetailsScreen extends React.Component {
      <View style={styles.container}>
       <View style={styles.welcomecontainer}>
         <Text style={styles.paragraph}>
-         Nome: Letícia de Lima Siqueira
+         Nome: Mariana Oliveira
         </Text>
          <Text style={styles.paragraph}>
-		    Matrícula: 0050012430
+		    Matrícula: 0050013985
 		    </Text>
         <Text style={styles.paragraph}>
-		   Link GitHub:github.com/leticials1/ipi-uni-2018.2
+		   Link GitHub:github.com/mariana1912/ipi-uni-2018.2
 		    </Text>
         
         </View>
@@ -70,36 +70,3 @@ const RootStack = createStackNavigator(
   }
 );
 
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#333',
-  },
- welcomecontainer: { 
-	margin: 20,
-	padding: 20,
-  backgroundColor: "#FFF",
-	borderRadius: 3,
-	alignItems: 'center',
-  justifyContent: 'center',
-  },
-  welcomeDescription: {
-    color: "#666",
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    
-  },
-});
